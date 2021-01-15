7289e7053fadb6329a443054630feb5f2041ccd1
Cristhian Maia <cristhian.smaia@gmail.com>
2021-01-15 14:02:59 -0300

    initial

---
+++ src/main/java/com/webharas/WebharasApplication.java
@@ -0,0 +1,13 @@
+package com.webharas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebharasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebharasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webharas/WebharasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webharas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebharasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
